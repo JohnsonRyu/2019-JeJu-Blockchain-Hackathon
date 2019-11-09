a1aad2bdd01b16f201aaab7f69d31f3eead006bb
DianaLeee <yeonylee94@gmail.com>
2019-11-10 05:31:02 +0900

    ENH: Change InitialPage as  indexPage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
-import HomePage from "./HomePage"
+// import HomePage from "./HomePage"
+
+// const Index = () => (
+//   <HomePage />
+// );
+
+// export default Index;
+
+
+
+
+import PageLayout from "../layouts/PageLayout"
+import Initial from "../components/InitialPage/Initial"
+import NonAuthUserHeader from "../components/UserHeader/NoAuthUserHeader"
+import NavTopHeader from "../components/Common/NavTopHeader"
+import NavBottomFooter from "../components/Common/NavBottomFooter"
 
 const Index = () => (
-  <HomePage />
+  <PageLayout headerTitle=" " isBack={false} >
+    {/* <NavTopHeader title="댕댕 VISA" /> */}
+    <NonAuthUserHeader />
+    <Initial />
+    {/* <NavBottomFooter /> */}
+  </PageLayout>
 );
-
 export default Index;
